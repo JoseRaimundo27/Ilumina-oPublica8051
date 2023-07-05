94e8b6ccf45a0d2df4a10d7bffe79988ec429397
José Notebook <joseraimundoalves@hotmail.com>
2023-07-05 17:29:43 -0300

    Display LCD funcionando

--- Trabalho2/codigos-arduino-C/CodigoArduino/CodigoArduino.ino
+++ Trabalho2/codigos-arduino-C/CodigoArduino/CodigoArduino.ino
@@ -1,4 +1,8 @@
+#include <LiquidCrystal.h>
+LiquidCrystal lcd(10,9,8,7,6,5);
+
 //DEFINE PORTAS DOS SENSORES/LED/BOTAO:
+
 #define ldr A0
 #define temp A2
 #define amp A3
@@ -7,11 +11,11 @@
 #define btn 11 // Int 0 = pino 2
 
 //DEFINE SAIDA DOS SENSORES
-#define saida_ldr 12 
-#define saidaSensores 4
-#define saida_temp 10
-#define saida_amp 9
-#define saida_volt 6
+int saida_ldr = 12;
+int saidaSensores = 4;
+int saida_temp = 10;
+int saida_amp = 9;
+int saida_volt = 8;
 
 
 //VARIAVEIS AUXILIARES:
@@ -20,6 +24,8 @@ int status1=1; // status1 inicia em 1 ( led começa ligado )
 int status2=1; // status2 inicia em 1 ( led começa ligado )
 
 void setup() {
+  //LCD:
+  lcd.begin(16,2);
  // INTERRUPÇÕES:
   attachInterrupt(0,intBotaoLdr,CHANGE); // Configurando interrupção botao mudança de borda
   attachInterrupt(1,intSensores, CHANGE); // Configurando interrupção mudança de borda
@@ -31,9 +37,10 @@ void setup() {
   pinMode(ldr, INPUT); // Sensor de luz como entrada
   pinMode(saida_ldr, OUTPUT); // Sensor ldr como saída
   pinMode(saidaSensores, OUTPUT); // Saída sensores como saída
+  pinMode(saida_temp, OUTPUT);
+  pinMode(saida_amp, OUTPUT);
+  pinMode(saida_volt, OUTPUT);
   
-
-
   // SERIAL:
   Serial.begin(9600);  
 }
@@ -65,19 +72,37 @@ void intBotaoLdr(){
     ligaLed();
     status2 = 1; //estava desligado, vou ligar
   }
-  
 }
 
 //LOOP:
 void loop() { 
   //Recebendo e convertendo valores analogicos:
-  valor_temp = (analogRead(temp)/2 - 1); //Convertendo valor temp
+  valor_temp = ((analogRead(temp))/2-1); //Convertendo valor temp
   valor_volt = (analogRead(volt)/2); // convertendo voltagem, n ficou exato
   valor_amp = (analogRead(amp)/2); // não converti amper
-  valor_ldr = analogRead(ldr);
 
+  analogWrite(5, valor_volt/723);
+  analogWrite(saida_volt, valor_volt/4);
+  analogWrite(saida_amp, valor_amp/512);
+  
+  //PLOTANDO SENSORES NO LCD:
+  lcd.setCursor(0,0);
+  lcd.print("T=");
+  lcd.setCursor(2,0);
+  lcd.print(valor_temp);
+  lcd.setCursor(5,0);
+  lcd.print("V=");
+  lcd.setCursor(7,0);
+  lcd.print(valor_volt);
+  lcd.setCursor(11,0);
+  lcd.print("I=");
+  lcd.setCursor(13,0);
+  lcd.print(valor_amp);
+  
+
+  
   //Serial:
-  Serial.println((analogRead(temp)/2) - 1 );
+  Serial.println(analogRead(5) ); 
   delay(100);
 
   //Rotina principal:
