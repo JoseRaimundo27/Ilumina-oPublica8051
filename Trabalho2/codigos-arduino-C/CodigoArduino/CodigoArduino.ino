8c548dd6ebbaccd8a659b364301bf79415fca3cf
José Notebook <joseraimundoalves@hotmail.com>
2023-07-05 11:23:45 -0300

    ligando e desligando led com interrupção para ldr e botão

--- Trabalho2/codigos-arduino-C/CodigoArduino/CodigoArduino.ino
+++ Trabalho2/codigos-arduino-C/CodigoArduino/CodigoArduino.ino
@@ -1,17 +1,34 @@
+//DEFINE PORTAS DOS SENSORES/LED/BOTAO:
 #define ldr A0
 #define temp A2
 #define amp A3
 #define volt A5
 #define led 13
-#define btn 2
+#define btn 11 // Int 0 = pino 2
+
+//DEFINE SAIDA DOS SENSORES
+#define saida_ldr 12 
+
+//VARIAVEIS AUXILIARES:
+int valor_temp, valor_ldr, valor_amp, valor_volt; // Valores dos sensores
+int status=1; // status inicia em 1 ( led começa ligado)
 
 void setup() {
+ // INTERRUPÇÕES:
+  attachInterrupt(0,intBotaoLdr,CHANGE); // Configurando interrupção botao borda de descida
+  attachInterrupt(1,intSensores, CHANGE); // Configurando interrupção mudança de borda
+
+  //PINOS: 
   pinMode(led, OUTPUT);//Pino 13 como saida do led
   pinMode(btn,INPUT);//Botao como entrada de dados
   pinMode(volt,INPUT);// Sensor de voltagem
   pinMode(ldr, INPUT); // Sensor de luz
-  Serial.begin(9600);
+  pinMode(saida_ldr, OUTPUT);
   
+
+
+  // SERIAL:
+  Serial.begin(9600);  
 }
 void ligaLed(){
   digitalWrite(led, HIGH);  
@@ -20,13 +37,43 @@ void apagaLed(){
   digitalWrite(led, LOW); 
 }
 
-void loop() {   
-  Serial.println(analogRead(amp));
-  delay(100);
-  if(digitalRead(btn) == HIGH ){ //Se botão for apertado apaga led
-      apagaLed();  // 
-  }else{
-      ligaLed();
+//FUNÇÕES DAS INTERRUPÇÕES:
+void intBotaoLdr(){
+  if(status == 1){
+    apagaLed();
+    status = 0;
+  }
+  else{
+    apagaLed();
+    status = 1;
   }
   
+  
+}
+void intSensores(){
+  apagaLed();
+  }
+
+//LOOP:
+void loop() { 
+  //Recebendo e convertendo valores analogicos:
+  valor_temp = (analogRead(temp)/2 - 1); //Convertendo valor temp
+  valor_volt = (analogRead(volt)/2); // convertendo voltagem, n ficou exato
+  valor_amp = analogRead(amp); // não converti amper
+  valor_ldr = analogRead(ldr);
+
+  //Serial:
+  Serial.println((analogRead(ldr)) );
+  delay(100);
+
+  //Rotina principal:
+  if(status == 1){
+    ligaLed(); //Led ligado por default
+  }
+
+  if(valor_ldr > 49){
+    digitalWrite(saida_ldr, HIGH);
+  }else {
+    digitalWrite(saida_ldr, LOW);
+  }
 }
